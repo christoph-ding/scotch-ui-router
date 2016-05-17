c0fd351ce4ce8f239880ce2351ffce0819e7bb83
Pokegear <christoph.ding@gmail.com>
2016-05-16 21:56:27 -0400

    Added partial views to home page.

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ routerApp.config(function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/home');
 
   $stateProvider
-    // Home state
+      // Home state and nested views
       .state('home', {
         url: '/home',
-        template: 'partial-home.html'
-      });
-    // About page and multiple named views
-      // .state('about', 
-      // // {
-      // //   url: '',
-      // //   template: ''
-      // // }
-      // );
+        templateUrl: 'partial-home.html'
+      })
+      
+      // nested list with custom controller
+      .state('home.list', {
+          url: '/list',
+          templateUrl: 'partial-home-list.html',
+          controller: function($scope) {
+              $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+          }
+      })
+      // nested list with just some random string data
+      .state('home.paragraph', {
+          url: '/paragraph',
+          template: 'I could sure use a drink right now.'
+      })
 });
