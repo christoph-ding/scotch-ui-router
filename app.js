2aa8e18421ebcf740eca0d6b584c00e150c9fd41
Pokegear <christoph.ding@gmail.com>
2016-05-16 22:24:51 -0400

    Finished website

--- app.js
+++ app.js
@@ -23,4 +23,46 @@ routerApp.config(function ($stateProvider, $urlRouterProvider) {
           url: '/paragraph',
           template: 'I could sure use a drink right now.'
       })
+
+      .state('about', {
+          url: '/about',
+          views: {
+
+              // the main template will be placed here (relatively named)
+              '': { templateUrl: 'partial-about.html' },
+
+              // the child views will be defined here (absolutely named)
+              'columnOne@about': { template: 'Look I am a column!' },
+
+              // for column two, we'll define a separate controller 
+              'columnTwo@about': { 
+                  templateUrl: 'table-data.html',
+                  controller: 'scotchController'
+              }
+          }
+          
+      });
+
+});
+
+// let's define the scotch controller that we call up in the about state
+routerApp.controller('scotchController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
+    
 });
